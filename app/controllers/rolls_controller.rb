8d076b0ef2e8ff1a7b08c874156f1a1721193bc7
Clay Grumieaux <claygrumeo@gmail.com>
2023-04-10 00:58:45 -0400

    [CG] - small tweaks / Rolls page detail

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -3,6 +3,7 @@ class RollsController < ApplicationController
   # Populate the rolls that will be listed on the page
   def index
     @user = helpers.current_user
-    @rolls = @user.rolls
+    @rolls = @user.rolls.order(created_at: :desc)
+    @rolls_count = @rolls.count
   end
 end
